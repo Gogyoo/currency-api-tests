beba8c95fc07cf371d3476d4095bf3fc7dcf498d
gogyoo <pourriel1@hotmail.com>
2026-07-28 20:12:14 +0200

    Added happy path function to client.py

--- api/client.py
+++ api/client.py
@@ -8,12 +8,13 @@ APP_ID = os.getenv("APP_ID")
 baseURL = "https://openexchangerates.org/api/"
 params = {"app_id": APP_ID}
 
-# First we want to test if our token works, and whether it was found by our main file
-# by sending a simple request and check we get a code 200.
-# The /latest.json endpoint is as good as any to start with.
+def happy_path():
+    """First we want to test if our token works, and we get a code 200.
+    The /latest.json endpoint is the simplest one to start with."""
 
-response = requests.get(f"{baseURL}latest.json", params=params)
-latest_json = response.json()
-print(f"Status code: {response.status_code}")
-print(f"Base currency of query: {latest_json["base"]}")
-print(f"Most recently, 1 USD is worth {latest_json["rates"]["EUR"]} EUR.")
+    happy_response = requests.get(f"{baseURL}latest.json", params=params)
+    latest_json = happy_response.json()
+
+    assert happy_response.status_code == 200
+
+happy_path()
