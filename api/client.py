418a0e5eb0671310867f1e542f0f5217971ceb70
gogyoo <pourriel1@hotmail.com>
2026-07-28 20:17:03 +0200

    Checked happy path for sub-second response time

--- api/client.py
+++ api/client.py
@@ -9,12 +9,14 @@ baseURL = "https://openexchangerates.org/api/"
 params = {"app_id": APP_ID}
 
 def happy_path():
-    """First we want to test if our token works, and we get a code 200.
+    """First we want to test if our token works, we get a code 200,
+    and a few other checks that makes sense for our happy path.
     The /latest.json endpoint is the simplest one to start with."""
 
     happy_response = requests.get(f"{baseURL}latest.json", params=params)
     latest_json = happy_response.json()
 
     assert happy_response.status_code == 200
+    assert happy_response.elapsed.total_seconds() < 1
 
 happy_path()
